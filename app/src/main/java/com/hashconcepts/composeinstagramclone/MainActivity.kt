5371af73c2ea2787ad430d68e0fa0bb50905588a
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2022-07-28 15:00:59 +0100

    added dependencies

--- app/src/main/java/com/hashconcepts/composeinstagramclone/MainActivity.kt
+++ app/src/main/java/com/hashconcepts/composeinstagramclone/MainActivity.kt
@@ -22,22 +22,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeInstagramCloneTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/hashconcepts/composeinstagramclone/ui/theme/Type.kt
+++ app/src/main/java/com/hashconcepts/composeinstagramclone/ui/theme/Type.kt
@@ -2,23 +2,43 @@ package com.hashconcepts.composeinstagramclone.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hashconcepts.composeinstagramclone.R
 
+val sfUIDisplay = FontFamily(
+    listOf(
+        Font(R.font.sf_ui_display_bold),
+        Font(R.font.sf_ui_display_medium),
+        Font(R.font.sf_ui_display_light),
+    )
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = sfUIDisplay,
+        fontWeight = FontWeight.Light,
+        fontSize = 15.sp
     ),
+    h1 = TextStyle(
+        fontFamily = sfUIDisplay,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = sfUIDisplay,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    button = TextStyle(
+        fontFamily = sfUIDisplay,
+        fontWeight = FontWeight.Bold,
+        fontSize = 13.sp
+    ),
+    /* Other default text styles to override
+
     caption = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
