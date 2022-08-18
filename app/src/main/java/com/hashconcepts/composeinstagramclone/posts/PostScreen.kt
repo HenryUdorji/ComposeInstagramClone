7abffad12ef0ac5a25c4e735a31a08fdfa409e00
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2022-08-18 20:25:12 +0100

    working on post screen

--- app/src/main/java/com/hashconcepts/composeinstagramclone/posts/PostScreen.kt
+++ app/src/main/java/com/hashconcepts/composeinstagramclone/posts/PostScreen.kt
@@ -8,12 +8,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hashconcepts.composeinstagramclone.R
 import com.hashconcepts.composeinstagramclone.ui.theme.FormFieldBgDark
@@ -41,19 +42,40 @@ fun PostScreen(
             }
         }
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .clip(RoundedCornerShape(20.dp))
-                .background(FormFieldBgDark)
-                .padding(10.dp)
-                .align(Alignment.BottomEnd)
-        ) {
-            Text(text = "POST", style = MaterialTheme.typography.body1, color = Color.White)
-            Spacer(modifier = Modifier.width(10.dp))
-            Text(text = "STORY", style = MaterialTheme.typography.body1, color = Color.White)
-            Spacer(modifier = Modifier.width(10.dp))
-            Text(text = "REEL", style = MaterialTheme.typography.body1, color = Color.White)
+        var option by remember { mutableStateOf("POST") }
+        BottomOptionSection(
+            selectedItem = option,
+            onOptionSelected = { option = it }
+        )
+    }
+}
+
+@Composable
+fun BoxScope.BottomOptionSection(
+    onOptionSelected: (String) -> Unit,
+    selectedItem: String,
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(10.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .background(FormFieldBgDark.copy(alpha = 0.8f))
+            .padding(horizontal = 5.dp)
+            .align(Alignment.BottomEnd)
+    ) {
+        val options = listOf("POST", "STORY", "REEL")
+
+        options.forEach { option ->
+            Text(
+                text = option,
+                style = MaterialTheme.typography.body1,
+                color = if (selectedItem == option) Color.White else Color.Gray,
+                modifier = Modifier
+                    .padding(5.dp)
+                    .clickable { onOptionSelected(option) }
+            )
         }
     }
 }
